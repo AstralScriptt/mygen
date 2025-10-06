b81a545c602e4ed7461c4e2cbcaed7ce7005f3a5
liam675 <liamacosta629@gmail.com>
2025-10-06 13:02:37 -0400

    Update stock.js

--- stock.js
+++ stock.js
@@ -1,3 +1,15 @@
-// stock.js - Hard reset, empty stock
-let stock = [];
+// stock.js - Dummy stock for testing
+let stock = [
+    { username: 'alt1_roblox', password: 'pass123' },
+    { username: 'alt2_roblox', password: 'pass456' },
+    { username: 'alt3_roblox', password: 'pass789' },
+    { username: 'alt4_roblox', password: 'passabc' },
+    { username: 'alt5_roblox', password: 'passdef' },
+    { username: 'alt6_roblox', password: 'passghi' },
+    { username: 'alt7_roblox', password: 'passjkl' },
+    { username: 'alt8_roblox', password: 'passmno' },
+    { username: 'alt9_roblox', password: 'passpqr' },
+    { username: 'alt10_roblox', password: 'passstu' },
+    // Add more as needed
+];
 localStorage.setItem('sharedStock', JSON.stringify(stock));
