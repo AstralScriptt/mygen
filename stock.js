0de9b1a828e7cbe881b83a4898306560cab3f788
liam675 <liamacosta629@gmail.com>
2025-10-06 12:16:30 -0400

    Update stock.js

--- stock.js
+++ stock.js
@@ -1,6 +1,6 @@
-// stock.js - Add your alt accounts here
+// stock.js
 let stock = [
     { username: "RawrAlt1", password: "pass123" },
     { username: "RawrAlt2", password: "secure456" },
-    // Add more: { username: "...", password: "..." },
+    // Add more accounts here
 ];
